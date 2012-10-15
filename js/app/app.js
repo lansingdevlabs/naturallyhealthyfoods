ffa73de320869360c4bc37b4cac9b36519701a36
Brian <briancherron@gmail.com>
2012-10-15 17:39:50 -0400

    adding ember.js

---
+++ js/app/app.js
@@ -0,0 +1,82 @@
+// create application
+App = Em.Application.create();
+
+App.ApplicationView = Em.View.extend({
+	templateName: "applicationTemplate"
+});
+
+// views
+App.HomeView = Em.View.extend({
+	templateName: "homeTemplate"
+});
+
+App.AboutView = Em.View.extend({
+	templateName: "aboutTemplate"
+});
+
+App.ProductsView = Em.View.extend({
+	templateName: "productsTemplate"
+});
+
+App.ContactView = Em.View.extend({
+	templateName: "contactTemplate"
+});
+
+// controllers
+App.ApplicationController = Em.Controller.extend({
+
+});
+
+App.HomeController = Em.Controller.extend({
+
+});
+
+App.AboutController = Em.Controller.extend({
+
+});
+
+App.ProductsController = Em.Controller.extend({
+
+});
+
+App.ContactController = Em.Controller.extend({
+
+});
+
+// router
+App.Router =  Em.Router.extend({
+	root: Em.Route.extend({
+		goHome: Em.Route.transitionTo("home"),
+		goAbout: Em.Route.transitionTo("about"),
+		goProducts: Em.Route.transitionTo("products"),
+		goContact: Em.Route.transitionTo("contact"),
+		home: Em.Route.extend({
+			route: "/",
+			connectOutlets: function (router, context) {
+				router.get("applicationController").connectOutlet("home");
+			}
+		}),
+		about: Em.Route.extend({
+			route: "/about",
+			connectOutlets: function (router, context) {
+				router.get("applicationController").connectOutlet("about");
+			}
+		}),
+		products: Em.Route.extend({
+			route: "/products",
+			connectOutlets: function (router, context) {
+				router.get("applicationController").connectOutlet("products");
+			}
+		}),
+		contact: Em.Route.extend({
+			route: "/contact",
+			connectOutlets: function (router, context) {
+				router.get("applicationController").connectOutlet("contact");
+			}
+		})
+	})
+});
+
+// initialize application
+App.initialize();
+
