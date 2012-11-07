60f01cf1173dffb7a582aaf08e104e8004d3261c
Brian <briancherron@gmail.com>
2012-11-06 22:39:31 -0500

    products page UI

--- js/app/app.js
+++ js/app/app.js
@@ -25,6 +25,10 @@ App.ProductsView = Em.View.extend({
 	templateName: "productsTemplate"
 });
 
+App.ProductView = Em.View.extend({
+	templateName: "productTemplate"
+})
+
 App.ContactView = Em.View.extend({
 	templateName: "contactTemplate"
 });
@@ -47,8 +51,95 @@ App.MachineController = Em.Controller.extend({
 
 });
 
-App.ProductsController = Em.Controller.extend({
+App.Product = Em.Object.extend({
+	name: "",
+	description: "",
+	url: "",
+	spriteClass: ""
+});
 
+App.ProductsController = Em.ArrayController.extend({
+	content: [
+		App.Product.create({
+			name: "Annie's Homegrown Cheddar Bunnies",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.annies.com/",
+			spriteClass: "annies-homegrown"
+		}),
+		App.Product.create({
+			name: "Brothers-All-Natural Fruit Crisps",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.brothersallnatural.com/",
+			spriteClass: "brothers-all-natural"
+			
+		}),
+		App.Product.create({
+			name: "Clif Bar",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.clifbar.com/",
+			spriteClass: "clif-bar"
+		}),
+		App.Product.create({
+			name: "Clif Mojo Bar",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.clifbar.com/",
+			spriteClass: "clif-mojo-bar"
+		}),
+		App.Product.create({
+			name: "Clif Kid Twisted Fruit",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.clifbar.com/",
+			spriteClass: "clif-twisted-fruit"
+		}),
+		App.Product.create({
+			name: "Clif Kid Z Bar",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.clifbar.com/",
+			spriteClass: "clif-z-bar"
+		}),
+		App.Product.create({
+			name: "Glaceau Vitamin Water",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.vitaminwater.com/",
+			spriteClass: "glaceau-vitamin-water"
+		}),
+		App.Product.create({
+			name: "Kashi TLC Cruncy Granola Bar",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.kashi.com/",
+			spriteClass: "kashi-tlc-crunchy-granola-bar"
+		}),
+		App.Product.create({
+			name: "Luna Bar",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.lunabar.com/",
+			spriteClass: "luna-bar"
+		}),
+		App.Product.create({
+			name: "Materne GoGo Squeez",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.gogosqueez.com/",
+			spriteClass: "materne-gogo-squeez"
+		}),
+		App.Product.create({
+			name: "Stacy's All-Natural Pita Chips",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.stacyssnacks.com/",
+			spriteClass: "stacys-all-natural-pita-chips"
+		}),
+		App.Product.create({
+			name: "Stretch Island Fruit Leather",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.stretchislandfruit.com/",
+			spriteClass: "stretch-island-fruit-leather"
+		}),
+		App.Product.create({
+			name: "V8 Fusion",
+			description: "Cupcake ipsum dolor sit amet chocolate cupcake sugar plum. Bonbon chocolate lollipop powder wypas sweet brownie gingerbread muffin. Lemon drops chupa chups gummies ice cream toffee donut topping. Cookie pie apple pie marshmallow sugar plum lollipop oat cake I love fruitcake. Tootsie roll wafer liquorice dessert jelly. Halvah fruitcake biscuit croissant sugar plum. I love jelly-o icing topping biscuit liquorice. Tiramisu lemon drops ice cream cake candy canes cheesecake. I love pudding cotton candy.",
+			url: "http://www.v8juice.com/",
+			spriteClass: "v8-fusion"
+		})
+	]
 });
 
 App.ContactController = Em.Controller.extend({
